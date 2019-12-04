b3e8d28489c429043fddf8805430ab41a2b9cc18
Greg Wood <contact@gregwood.io>
2019-12-04 18:41:46 -0500

    Day 04 part two.

--- 2019/day04/day04.go
+++ 2019/day04/day04.go
@@ -4,7 +4,7 @@ func solvePartOne(from, to int) int {
 	count := 0
 
 	for i := from; i <= to; i++ {
-		if isValid(i) {
+		if isValid(i, false) {
 			count++
 		}
 	}
@@ -12,29 +12,51 @@ func solvePartOne(from, to int) int {
 	return count
 }
 
-func isValid(num int) bool {
+func solvePartTwo(from, to int) int {
+	count := 0
+
+	for i := from; i <= to; i++ {
+		if isValid(i, true) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func isValid(num int, isPartTwo bool) bool {
 	digits := getDigits(num)
+	countDigits := [10]int{}
 
 	if len(digits) != 6 {
 		return false
 	}
 
-	hasDouble := false
 	for i := 0; i < len(digits)-1; i++ {
 		if digits[i] > digits[i+1] {
 			return false
 		}
 
-		if digits[i] == digits[i+1] {
-			hasDouble = true
+		countDigits[digits[i]]++
+	}
+
+	countDigits[digits[5]]++
+
+	if !isPartTwo {
+		for _, val := range countDigits {
+			if val >= 2 {
+				return true
+			}
+		}
+	} else {
+		for _, val := range countDigits {
+			if val == 2 {
+				return true
+			}
 		}
 	}
 
-	if !hasDouble {
-		return false
-	}
-
-	return true
+	return false
 }
 
 func getDigits(num int) []int {
--- 2019/day04/day04_test.go
+++ 2019/day04/day04_test.go
@@ -39,7 +39,7 @@ func TestIsValid(t *testing.T) {
 	}
 
 	for _, td := range testDatas {
-		actual := isValid(td.input)
+		actual := isValid(td.input, false)
 		if actual != td.expected {
 			t.Errorf("Expected %t but was %t\n for value %d", td.expected, actual, td.input)
 		}
@@ -52,5 +52,38 @@ func TestSolvePartOne(t *testing.T) {
 	}
 
 	result := solvePartOne(240298, 784956)
+	fmt.Println(result)
+}
+
+func TestIsValidPartTwo(t *testing.T) {
+	testDatas := []testData{
+		testData{
+			input:    112233,
+			expected: true,
+		},
+		testData{
+			input:    123444,
+			expected: false,
+		},
+		testData{
+			input:    111122,
+			expected: true,
+		},
+	}
+
+	for _, td := range testDatas {
+		actual := isValid(td.input, true)
+		if actual != td.expected {
+			t.Errorf("Expected %t but was %t\n for value %d", td.expected, actual, td.input)
+		}
+	}
+}
+
+func TestSolvePartTwo(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping actual solution run.")
+	}
+
+	result := solvePartTwo(240298, 784956)
 	fmt.Println(result)
 }
