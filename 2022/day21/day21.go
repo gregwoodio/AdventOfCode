60536f1c7c323bc7b1d310248192204333de781b
Greg Wood <contact@gregwood.io>
2022-12-21 11:46:17 -0500

    Day 21 part 2

--- 2022/day21/day21.go
+++ 2022/day21/day21.go
@@ -10,6 +10,7 @@ type monkey struct {
 	id        string
 	num       *int
 	operation func(a, b int) int
+	op        string
 	operand   []string
 }
 
@@ -36,7 +37,112 @@ func solvePartOne(input []string) int {
 }
 
 func solvePartTwo(input []string) int {
-	return -1
+	monkeys := parse(input)
+	monkeys["humn"].num = nil
+	root := monkeys["root"]
+	// check equality of operands
+	m1, ok := monkeys[root.operand[0]]
+	if !ok {
+		panic(fmt.Sprintf("Missing monkey %s", root.operand[0]))
+	}
+	m2, ok := monkeys[root.operand[1]]
+	if !ok {
+		panic(fmt.Sprintf("Missing monkey %s", root.operand[1]))
+	}
+
+	m1Path := findHumnPath(m1, monkeys)
+	m2Path := findHumnPath(m2, monkeys)
+	var result int
+	var op string
+	if m1Path {
+		calc(m2, monkeys)
+		result = *m2.num
+		op = m1.op
+		m1, m2 = monkeys[m1.operand[0]], monkeys[m1.operand[1]]
+	} else {
+		calc(m1, monkeys)
+		result = *m1.num
+		op = m2.op
+		m1, m2 = monkeys[m2.operand[0]], monkeys[m2.operand[1]]
+	}
+
+	humn := monkeys["humn"]
+	for {
+		// find humn path
+		m1Path = findHumnPath(m1, monkeys)
+		m2Path = findHumnPath(m2, monkeys)
+
+		// reverse the operation for the humn path
+		if m1Path {
+			calc(m2, monkeys)
+			switch op {
+			case "/":
+				sum := mul(*m2.num, result)
+				m1.num = &sum
+			case "-":
+				sum := add(*m2.num, result)
+				m1.num = &sum
+			case "*":
+				sum := div(result, *m2.num)
+				m1.num = &sum
+			case "+":
+				sum := sub(result, *m2.num)
+				m1.num = &sum
+			}
+			result = *m1.num
+			op = m1.op
+
+			if humn.num != nil {
+				return *humn.num
+			}
+
+			m1, m2 = monkeys[m1.operand[0]], monkeys[m1.operand[1]]
+
+		} else if m2Path {
+			calc(m1, monkeys)
+			switch op {
+			case "/":
+				sum := div(*m1.num, result)
+				m2.num = &sum
+			case "-":
+				sum := sub(*m1.num, result)
+				m2.num = &sum
+			case "*":
+				sum := div(result, *m1.num)
+				m2.num = &sum
+			case "+":
+				sum := sub(result, *m1.num)
+				m2.num = &sum
+			}
+			result = *m2.num
+			op = m2.op
+
+			if humn.num != nil {
+				return *humn.num
+			}
+
+			m1, m2 = monkeys[m2.operand[0]], monkeys[m2.operand[1]]
+		}
+	}
+}
+
+func findHumnPath(m *monkey, monkeys map[string]*monkey) bool {
+	if m.id == "humn" {
+		return true
+	}
+
+	if len(m.operand) > 0 {
+		if m.operand[0] == "humn" || m.operand[1] == "humn" {
+			return true
+		} else {
+			m1 := findHumnPath(monkeys[m.operand[0]], monkeys)
+			m2 := findHumnPath(monkeys[m.operand[1]], monkeys)
+
+			return m1 || m2
+		}
+	}
+
+	return false
 }
 
 func calc(m *monkey, monkeys map[string]*monkey) int {
@@ -72,6 +178,7 @@ func parse(input []string) map[string]*monkey {
 			// operation
 			ops := strings.Split(parts[1], " ")
 			m.operand = []string{ops[0], ops[2]}
+			m.op = ops[1]
 			switch ops[1] {
 			case "+":
 				m.operation = add
--- 2022/day21/day21_test.go
+++ 2022/day21/day21_test.go
@@ -44,10 +44,37 @@ func Test2022Day21_SolvePartOneActual(t *testing.T) {
 
 func Test2022Day21_SolvePartTwo(t *testing.T) {
 	inputs := []string{
-		"foo",
+		"root: pppw + sjmn",
+		"dbpl: 5",
+		"cczh: sllz + lgvd",
+		"zczc: 2",
+		"ptdq: humn - dvpt",
+		"dvpt: 3",
+		"lfqf: 4",
+		"humn: 5",
+		"ljgn: 2",
+		"sjmn: drzm * dbpl",
+		"sllz: 4",
+		"pppw: cczh / lfqf",
+		"lgvd: ljgn * ptdq",
+		"drzm: hmdt - zczc",
+		"hmdt: 32",
+
+		// // test 2, should be 3
+		// "root: bbbb + aaaa",
+		// "aaaa: 10",
+		// "bbbb: dddd + cccc",
+		// "cccc: 6",
+		// "dddd: ffff * eeee",
+		// "eeee: 4",
+		// "ffff: gggg - hhhh",
+		// "gggg: 16",
+		// "hhhh: iiii / humn",
+		// "iiii: 45",
+		// "humn: 42",
 	}
 
-	expected := -1
+	expected := 301
 	actual := solvePartTwo(inputs)
 	if actual != expected {
 		t.Errorf("Expected %d but was %d", expected, actual)
