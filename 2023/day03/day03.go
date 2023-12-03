51e8c23dc1c7c75be9a58fe37562234b9cec62ac
Greg Wood <contact@gregwood.io>
2023-12-03 13:47:18 -0500

    2023 Day 3 part 2

--- 2023/day03/day03.go
+++ 2023/day03/day03.go
@@ -1,6 +1,7 @@
 package day03
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 	"unicode"
@@ -60,9 +61,88 @@ func solvePartOne(input []string) int {
 	}
 
 	return sum
-
 }
 
 func solvePartTwo(input []string) int {
-	return -1
+	sum := 0
+
+	gear := "*"
+	gearNums := map[string][]int{}
+
+	for row, line := range input {
+		num := 0
+		gearLocation := ""
+
+		for col, ch := range line {
+			if !unicode.IsDigit(ch) {
+				if gearLocation != "" {
+					if _, ok := gearNums[gearLocation]; !ok {
+						gearNums[gearLocation] = []int{}
+					}
+					gearNums[gearLocation] = append(gearNums[gearLocation], num)
+				}
+
+				num = 0
+				gearLocation = ""
+			} else if unicode.IsDigit(ch) {
+				curr, _ := strconv.Atoi(string(ch))
+				num *= 10
+				num += curr
+
+				// check for surrounding symbols
+				if gearLocation == "" {
+					if row-1 >= 0 {
+						if col-1 >= 0 && strings.Contains(gear, string(input[row-1][col-1])) {
+							gearLocation = xyKey(col-1, row-1)
+						}
+						if strings.Contains(gear, string(input[row-1][col])) {
+							gearLocation = xyKey(col, row-1)
+						}
+						if col+1 < len(line) && strings.Contains(gear, string(input[row-1][col+1])) {
+							gearLocation = xyKey(col+1, row-1)
+						}
+					}
+					if col-1 >= 0 && strings.Contains(gear, string(input[row][col-1])) {
+						gearLocation = xyKey(col-1, row)
+					}
+					if col+1 < len(line) && strings.Contains(gear, string(input[row][col+1])) {
+						gearLocation = xyKey(col+1, row)
+					}
+
+					if row+1 < len(input) {
+						if col-1 >= 0 && strings.Contains(gear, string(input[row+1][col-1])) {
+							gearLocation = xyKey(col-1, row+1)
+						}
+						if strings.Contains(gear, string(input[row+1][col])) {
+							gearLocation = xyKey(col, row+1)
+						}
+						if col+1 < len(line) && strings.Contains(gear, string(input[row+1][col+1])) {
+							gearLocation = xyKey(col+1, row+1)
+						}
+					}
+				}
+			}
+		}
+
+		if gearLocation != "" {
+			if _, ok := gearNums[gearLocation]; !ok {
+				gearNums[gearLocation] = []int{}
+			}
+			gearNums[gearLocation] = append(gearNums[gearLocation], num)
+		}
+		num = 0
+		gearLocation = ""
+	}
+
+	for _, list := range gearNums {
+		if len(list) == 2 {
+			sum += list[0] * list[1]
+		}
+	}
+
+	return sum
+}
+
+func xyKey(x, y int) string {
+	return fmt.Sprintf("x:%d,y:%d", x, y)
 }
