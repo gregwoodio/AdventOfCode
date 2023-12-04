5732c1e902b638408900c41f4fec4db80bef4ee7
Greg Wood <contact@gregwood.io>
2023-12-04 08:09:24 -0500

    2023 Day 4 part 2

--- 2023/day04/day04.go
+++ 2023/day04/day04.go
@@ -1,6 +1,9 @@
 package day04
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 func solvePartOne(input []string) int {
 	sum := 0
@@ -32,6 +35,48 @@ func solvePartOne(input []string) int {
 	return sum
 }
 
+type scratchcard struct {
+	id        int
+	matches   int
+	instances int
+}
+
 func solvePartTwo(input []string) int {
-	return -1
+
+	cards := []scratchcard{}
+
+	for _, line := range input {
+		colon := strings.Index(line, ":")
+		id, _ := strconv.Atoi(line[strings.Index(line, " ")+1 : colon])
+
+		nums := strings.Split(line[colon+1:], "|")
+		winningNums := map[string]bool{}
+		for _, num := range strings.Split(nums[0], " ") {
+			if len(num) != 0 {
+				winningNums[num] = true
+			}
+		}
+		matches := 0
+		for _, num := range strings.Split(nums[1], " ") {
+			if _, ok := winningNums[num]; ok {
+				matches++
+			}
+		}
+
+		cards = append(cards, scratchcard{
+			id:        id,
+			matches:   matches,
+			instances: 1,
+		})
+	}
+
+	sum := 0
+	for i, card := range cards {
+		for j := 1; j <= card.matches; j++ {
+			cards[i+j].instances = cards[i+j].instances + card.instances
+		}
+		sum += card.instances
+	}
+
+	return sum
 }
--- 2023/day04/day04_test.go
+++ 2023/day04/day04_test.go
@@ -35,10 +35,15 @@ func Test2023Day04_SolvePartOneActual(t *testing.T) {
 
 func Test2023Day04_SolvePartTwo(t *testing.T) {
 	inputs := []string{
-		"foo",
+		"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+		"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+		"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+		"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+		"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+		"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
 	}
 
-	expected := -1
+	expected := 30
 	actual := solvePartTwo(inputs)
 	if actual != expected {
 		t.Errorf("Expected %d but was %d", expected, actual)
